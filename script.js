4842883b8aa0a6fbdfb5a9d1dde4882abc41fb2e
Kirill Karpov <kaarpusa2012@gmail.com>
2023-05-11 21:27:11 +0300

    усложненное задание

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 "use strict";
 
 function playGuessingGame() {
-  const secretNumber = 47;
-  
-  function askNumber(secretNumber) {
-    const userNumber = prompt("Угадай число от 1 до 100");
+  const maxAttempts = 10; 
+  let attemptsLeft = maxAttempts;
+  let secretNumber = generateSecretNumber();
+
+  function generateSecretNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  function askNumber() {
+    const userNumber = prompt(`Угадай число от 1 до 100. Осталось попыток: ${attemptsLeft}`);
 
     if (userNumber === null) {
       alert("Игра окончена");
+      return;
     } else if (isNaN(userNumber)) {
       alert("Введи число!");
-      askNumber(secretNumber);
     } else if (userNumber < 1 || userNumber > 100) {
       alert("Число должно быть от 1 до 100");
-      askNumber(secretNumber);
     } else if (userNumber < secretNumber) {
-      alert("Загаданное число больше");
-      askNumber(secretNumber);
+      alert(`Загаданное число больше. Осталось попыток: ${--attemptsLeft}`);
     } else if (userNumber > secretNumber) {
-      alert("Загаданное число меньше");
-      askNumber(secretNumber);
+      alert(`Загаданное число меньше. Осталось попыток: ${--attemptsLeft}`);
     } else {
       alert("Поздравляю, Вы угадали!!!");
+      if (confirm("Хотите сыграть еще?")) {
+        attemptsLeft = maxAttempts;
+        secretNumber = generateSecretNumber();
+      } else {
+        return;
+      }
     }
+
+    if (attemptsLeft === 0) {
+      if (confirm("Попытки закончились, хотите сыграть еще?")) {
+        attemptsLeft = maxAttempts;
+        secretNumber = generateSecretNumber();
+      } else {
+        return;
+      }
+    }
+
+    askNumber();
   }
 
-  return askNumber.bind(null, secretNumber);
+  return askNumber;
 }
 
 const startGame = playGuessingGame();
